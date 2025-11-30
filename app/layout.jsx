e184435993f5d9604b5e0028864f5d7ac07021c4
gamespiders <mygamesportal1@gmail.com>
2025-11-30 17:32:12 +0530

    Added AdSense script2

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import Sidebar from './Sidebar'
 import MobileMenu from './MobileMenu'
-import Head from 'next/head'
+import Script from "next/script"
 
 export const metadata = {
   title: "FileConverter",
@@ -11,14 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        {/* Google AdSense */}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2008367184647190"
-     crossorigin="anonymous"></script>
-      </Head>
-      <body className="min-h-screen bg-gray-50">
        
-        
+      <body className="min-h-screen bg-gray-50">
+ {/* Google AdSense */}
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2008367184647190"
+          crossOrigin="anonymous"
+        />
         {/* CLIENT: mobile menu button + mobile drawer */}
         <MobileMenu />
 
